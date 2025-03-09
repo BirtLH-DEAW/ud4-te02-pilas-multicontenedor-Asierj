8e8757d80d4422b4819de96723443c9b3cba3257
Ajaio <74831097+Asierj@users.noreply.github.com>
2025-03-09 14:36:18 +0100

    Delete ud4-te02-pilas-multicontenedor-Asierj directory

--- ud4-te02-pilas-multicontenedor-Asierj/README.md
+++
@@ -1,4 +0,0 @@
-# UD4-Multicontenedor_base
-TE02 - proyecto modificado
-
-Se ha modificado el proyecto para permitir el despliegue automatizado de ambos programas, Wordpress y el proyecto PHP anterior
--- ud4-te02-pilas-multicontenedor-Asierj/mysql/crearBBDDwordpress.sql
+++
@@ -1,5 +0,0 @@
--- Creamos la base de datos de Wordpress y asignamos los permisos
-CREATE DATABASE `wordpress` DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci;
-USE `wordpress`;
-GRANT ALL ON `wordpress`.*
-TO `dwes`;
